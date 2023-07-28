0a040cc295a993cad71ee25659a92c6eeeb08f8d
luffy <1527211162@qq.com>
2023-07-28 17:55:33 +0800

    ...

--- Alg/common/file_upload.py
+++ Alg/common/file_upload.py
@@ -1,8 +1,8 @@
 from qcloud_cos import CosConfig
 from qcloud_cos import CosS3Client
 
-secret_id = "AKIDMZYBaliRwDG5f2C97wZy84lEUXMyMirm"  # 替换为用户的 secretId
-secret_key = "lKfnjRwEH6h3B5uVs0ua2GG2a0FUYO0O"  # 替换为用户的 secretKey
+secret_id = #  # 替换为用户的 secretId
+secret_key = # # 替换为用户的 secretKey
 region = "ap-chengdu"  # 替换为用户的 Region
 config = CosConfig(Region=region, SecretId=secret_id, SecretKey=secret_key)
 client = CosS3Client(config)
